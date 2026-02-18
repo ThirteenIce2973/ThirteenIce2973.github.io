c55c9d8810d8abacea22df57e20a93d4b7ae2949
ThirteenIce2973 <2713281947@qq.com>
2026-02-18 18:10:10 +0800

    Site updated: 2026-02-18 18:10:08

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1771409027181";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1771409401690";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
